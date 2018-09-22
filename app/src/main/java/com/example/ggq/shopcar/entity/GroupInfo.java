9a9beca6b69a9d2f01b4478e6e7ece3d659babed
ggq <57042944@qq.com>
2018-09-22 11:05:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ggq/shopcar/entity/GroupInfo.java
@@ -0,0 +1,16 @@
+package com.example.ggq.shopcar.entity;
+
+public class GroupInfo extends BaseInfo
+{
+	public GroupInfo()
+	{
+		super();
+	}
+
+	public GroupInfo(String id, String name)
+	{
+		super(id, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
